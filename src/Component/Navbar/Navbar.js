7b6d406c446a9fcd48bb79c31abff95461358579
willyongaks <134366767+Devongaki@users.noreply.github.com>
2024-05-08 21:11:42 +0200

    components added
    Some of the main styling are applied as wel

---
+++ src/Component/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
