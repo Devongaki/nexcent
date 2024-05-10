9f6f897c1d31b6ad99da1a426dce8cbe69746ba4
willyongaks <134366767+Devongaki@users.noreply.github.com>
2024-05-10 14:14:32 +0200

    Update Hero.js

--- src/Component/HeroSection/Hero.js
+++ src/Component/HeroSection/Hero.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./Hero.css"
+import { NavLink } from 'react-router-dom';
+import HerImage from "../../Assets/Images/Illustration.png"
 
 function Hero() {
   return (
@@ -8,15 +10,20 @@ function Hero() {
         <div hero_text>
           <div className="hero_title">
             <h1>Lessons and insights from 8 years</h1>
+          </div>
+          <div className="hero_sub-title">
             <p>
               Where to grow your business as a photographer: site or social
               media?
             </p>
           </div>
-          <div className="hero_sub-title"></div>
-          <div className="hero_cta"></div>
+          <div className="hero_cta">
+            <NavLink to="/">Register</NavLink>
+          </div>
         </div>
-        <div hero_image></div>
+        <div hero_image>
+          <img src={HerImage} alt='Hero image' />
+        </div>
       </div>
     </div>
   );
