a646b9e8e344f97e08e1d946d4d12305ffbea6f9
willyongaks <134366767+Devongaki@users.noreply.github.com>
2024-05-21 16:33:58 +0200

    b

--- src/Component/Pages/Home/Achievements/Achievements.js
+++ src/Component/Pages/Home/Achievements/Achievements.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React from "react";
 import "./Achievements.css";
-import ArchiveImage1 from "../../../../Assets/Icons/Archievments/Vector.png"
+import { IoPeopleOutline } from "react-icons/io5";
+import { LiaHandshakeSolid } from "react-icons/lia";
+import { PiHandTap } from "react-icons/pi";
+import { MdOutlinePayment } from "react-icons/md";
 
 function Achievements() {
   return (
@@ -8,16 +11,47 @@ function Achievements() {
       <div className="achievements_container">
         <div className="archievment_heading">
           <h3>
-            Helping a local <span>business reinvent itself</span>{" "}
+            Helping a local 
+            <br></br>
+            <span>business reinvent itself</span>
           </h3>
           <p>We reached here with our hard work and dedication</p>
         </div>
-        <div className="archievment_icons">
-          <div className="archievment_icon">
-            <img src={ArchiveImage1} alt="Archievment icon" />
-            <div className="archievment_icon-info">
-              <p>2,245,341</p>
-              <p>Members</p>
+        <div className="achievement_icons-items">
+          <div className="achievement_icons-item">
+            <div className="achievment_icon">
+              <IoPeopleOutline />
+            </div>
+            <div className="achievment_icon-info">
+              <p className="achievment_icon-number">2,245,341</p>
+              <p className="achievment_icon-name">Members</p>
+            </div>
+          </div>
+          <div className="achievement_icons-item">
+            <div className="achievment_icon">
+              <LiaHandshakeSolid />
+            </div>
+            <div className="achievment_icon-info">
+              <p className="achievment_icon-number">46,328</p>
+              <p className="achievment_icon-name">Clubs</p>
+            </div>
+          </div>
+          <div className="achievement_icons-item">
+            <div className="achievment_icon">
+              <PiHandTap />
+            </div>
+            <div className="achievment_icon-info">
+              <p className="achievment_icon-number">828,867</p>
+              <p className="achievment_icon-name">Event Bookings</p>
+            </div>
+          </div>
+          <div className="achievement_icons-item">
+            <div className="achievment_icon">
+              <MdOutlinePayment />
+            </div>
+            <div className="achievment_icon-info">
+              <p className="achievment_icon-number">1,926,436</p>
+              <p className="achievment_icon-name">Payments</p>
             </div>
           </div>
         </div>
@@ -26,4 +60,4 @@ function Achievements() {
   );
 }
 
-export default Achievements
+export default Achievements;
