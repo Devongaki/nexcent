ea6744153978c9409cade715bfbcd65f023b71b6
willyongaks <134366767+Devongaki@users.noreply.github.com>
2024-05-27 17:32:00 +0200

    footer

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import "./Footer.css";
+import footerLogo from "../../Assets/Logo/Logolight.png";
 
 function Footer() {
   return (
-    <div className='footer_section container'>Footer</div>
+    <div className='footer_section container'>
+      <div className='footer_container'>
+        <div className='footer_left'>
+          <div className='footer_logo'>
+           <img src={footerLogo} />
+          </div>
+          <div className=''></div>
+          <div className=''></div>
+        </div>
+        <div className='footer_right'></div>
+      </div>
+    </div>
   )
 }
 
