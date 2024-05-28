d7c23710d7ea927ea278a22309de90edb359c9e9
willyongaks <134366767+Devongaki@users.noreply.github.com>
2024-05-28 16:36:00 +0200

    nn

--- src/Component/Pages/Home/Carousel/Carousel.js
+++ src/Component/Pages/Home/Carousel/Carousel.js
@@ -1,73 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Carousel.css";
 import {
   MdOutlineArrowBackIos,
   MdOutlineArrowForwardIos,
 } from "react-icons/md";
-
-const carouselItems = [
-  {
-    id: 1,
-    title: "LOREM IPSUM 01",
-    image:
-      "https://images.unsplash.com/photo-1516534775068-ba3e7458af70?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac ligula urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum malesuada pulvinar interdum.",
-  },
-  {
-    id: 2,
-    title: "LOREM IPSUM 02",
-    image:
-      "https://images.unsplash.com/photo-1542744173-8e7e53415bb0?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac ligula urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum malesuada pulvinar interdum.",
-  },
-  {
-    id: 3,
-    title: "LOREM IPSUM 03",
-    image:
-      "https://images.unsplash.com/photo-1534126511673-b6899657816a?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac ligula urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum malesuada pulvinar interdum.",
-  },
-  {
-    id: 6,
-    title: "LOREM IPSUM 06",
-    image:
-      "https://images.unsplash.com/photo-1506878206813-92402b8ded23?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac ligula urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum malesuada pulvinar interdum.",
-  },
-];
+import { carouselItems } from "./CarouselItems";
 
 function Carousel() {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [itemsToShow, setItemsToShow] = useState(3);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 600) {
+        setItemsToShow(1);
+      } else if (window.innerWidth <= 900) {
+        setItemsToShow(2);
+      } else {
+        setItemsToShow(3);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const handlePrevClick = () => {
     setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? Math.ceil(carouselItems.length / 3) - 1 : prevIndex - 1
+      prevIndex === 0
+        ? Math.ceil(carouselItems.length / itemsToShow) - 1
+        : prevIndex - 1
     );
   };
 
   const handleNextClick = () => {
     setCurrentIndex((prevIndex) =>
-      prevIndex === Math.ceil(carouselItems.length / 3) - 1 ? 0 : prevIndex + 1
+      prevIndex === Math.ceil(carouselItems.length / itemsToShow) - 1
+        ? 0
+        : prevIndex + 3
     );
   };
 
   return (
     <div className="carousel_section container">
+      <button
+        className="wiper-button wiper-button__left"
+        onClick={handlePrevClick}
+      >
+        <MdOutlineArrowBackIos />
+      </button>
       <div className="carousel_container">
-        <button
-          className="wiper-button wiper-button__left"
-          onClick={handlePrevClick}
-        >
-          <MdOutlineArrowBackIos />
-        </button>
         <div className="carousel_wrapper">
           <div
             className="carousel_items"
-            style={{ transform: `translateX(-${currentIndex * 33.3333}%)` }}
+            style={{ transform: `translateX(-${currentIndex * 100}%)` }}
           >
             {carouselItems.map((item) => (
               <div key={item.id} className="carousel_item">
@@ -95,13 +83,13 @@ function Carousel() {
             ))}
           </div>
         </div>
-        <button
-          className="wiper-button wiper-button__right"
-          onClick={handleNextClick}
-        >
-          <MdOutlineArrowForwardIos />
-        </button>
       </div>
+      <button
+        className="wiper-button wiper-button__right"
+        onClick={handleNextClick}
+      >
+        <MdOutlineArrowForwardIos />
+      </button>
     </div>
   );
 }
---
+++ src/Component/Pages/Home/Carousel/CarouselItems.js
@@ -0,0 +1,50 @@
+export const carouselItems = [
+  {
+    id: 1,
+    title: "LOREM IPSUM 01",
+    image:
+      "https://images.unsplash.com/photo-1516534775068-ba3e7458af70?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac ligula urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum malesuada pulvinar interdum.",
+  },
+  {
+    id: 2,
+    title: "LOREM IPSUM 02",
+    image:
+      "https://images.unsplash.com/photo-1542744173-8e7e53415bb0?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac ligula urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum malesuada pulvinar interdum.",
+  },
+  {
+    id: 3,
+    title: "LOREM IPSUM 03",
+    image:
+      "https://images.unsplash.com/photo-1502209877429-d7c6df9eb3f9?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac ligula urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum malesuada pulvinar interdum.",
+  },
+  {
+    id: 4,
+    title: "LOREM IPSUM 06",
+    image:
+      "https://images.unsplash.com/photo-1506878206813-92402b8ded23?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac ligula urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum malesuada pulvinar interdum.",
+  },
+  {
+    id: 4,
+    title: "LOREM IPSUM 06",
+    image:
+      "https://images.unsplash.com/photo-1506878206813-92402b8ded23?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac ligula urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum malesuada pulvinar interdum.",
+  },
+  {
+    id: 4,
+    title: "LOREM IPSUM 06",
+    image:
+      "https://images.unsplash.com/photo-1506878206813-92402b8ded23?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac ligula urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum malesuada pulvinar interdum.",
+  },
+];
