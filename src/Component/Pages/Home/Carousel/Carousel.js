d879946463a5c39c9246792c2a81c131e1d0f109
willyongaks <134366767+Devongaki@users.noreply.github.com>
2024-06-05 01:50:08 +0200

    carousel title

--- src/Component/Pages/Home/Carousel/Carousel.js
+++ src/Component/Pages/Home/Carousel/Carousel.js
@@ -28,54 +28,65 @@ function Carousel() {
 
   return (
     <div className="carousel_section container">
-      <button
-        className="wiper-button wiper-button__left"
-        onClick={handlePrevClick}
-      >
-        <MdOutlineArrowBackIos />
-      </button>
-      <div className="carousel_container">
-        <div className="carousel_wrapper">
-          <div
-            className="carousel_items"
-            style={{
-              transform: `translateX(-${currentIndex * (100 / 3)}%)`,
-              width: `calc(100% * ${carouselItems.length / 3});`,
-            }}
-          >
-            {carouselItems.map((item) => (
-              <div key={item.id} className="carousel_item">
-                <div className="carousel_image">
-                  <img src={item.image} alt={item.title} />
+      <div className="carousel_title">
+        <h3>Caring is the new marketing</h3>
+        <p>
+          The Nextcent blog is the best place to read about the latest
+          membership insights, trends and more. See who's joining the community,
+          read about how our community are increasing their membership income
+          and lot's more.​
+        </p>
+      </div>
+      <div className="carousel_content">
+        <button
+          className="wiper-button wiper-button__left"
+          onClick={handlePrevClick}
+        >
+          <MdOutlineArrowBackIos />
+        </button>
+        <div className="carousel_container">
+          <div className="carousel_wrapper">
+            <div
+              className="carousel_items"
+              style={{
+                transform: `translateX(-${currentIndex * (100 / 3)}%)`,
+                width: `calc(100% * ${carouselItems.length / 3});`,
+              }}
+            >
+              {carouselItems.map((item) => (
+                <div key={item.id} className="carousel_item">
+                  <div className="carousel_image">
+                    <img src={item.image} alt={item.title} />
+                  </div>
+                  <div className="carousel_text">
+                    <div className="carousel_text-title">
+                      <h5>{item.title}</h5>
+                    </div>
+                    <div className="carousel_text-description">
+                      <p>{item.description}</p>
+                    </div>
+                    <div className="carousel_text-button">
+                      <button
+                        onClick={() =>
+                          (window.location.href = `/blog/${item.id}`)
+                        }
+                      >
+                        Read More
+                      </button>
+                    </div>
+                  </div>
                 </div>
-                <div className="carousel_text">
-                  <div className="carousel_text-title">
-                    <h5>{item.title}</h5>
-                  </div>
-                  <div className="carousel_text-description">
-                    <p>{item.description}</p>
-                  </div>
-                  <div className="carousel_text-button">
-                    <button
-                      onClick={() =>
-                        (window.location.href = `/blog/${item.id}`)
-                      }
-                    >
-                      Read More
-                    </button>
-                  </div>
-                </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
+        <button
+          className="wiper-button wiper-button__right"
+          onClick={handleNextClick}
+        >
+          <MdOutlineArrowForwardIos />
+        </button>
       </div>
-      <button
-        className="wiper-button wiper-button__right"
-        onClick={handleNextClick}
-      >
-        <MdOutlineArrowForwardIos />
-      </button>
     </div>
   );
 }
