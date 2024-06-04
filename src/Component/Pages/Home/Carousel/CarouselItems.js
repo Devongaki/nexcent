92d2065fc1df0ac91bc167276ba2e8e5cf1b73f4
willyongaks <134366767+Devongaki@users.noreply.github.com>
2024-06-05 01:42:31 +0200

    images for the carousel

--- src/Component/Pages/Home/Carousel/CarouselItems.js
+++ src/Component/Pages/Home/Carousel/CarouselItems.js
@@ -27,20 +27,20 @@ export const carouselItems = [
     id: 4,
     title: "LOREM IPSUM 06",
     image:
-      "https://images.unsplash.com/photo-1506878206813-92402b8ded23?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1531482615713-2afd69097998?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac ligula urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum malesuada pulvinar interdum.",
   },
   {
-    id: 4,
+    id: 5,
     title: "LOREM IPSUM 06",
     image:
-      "https://images.unsplash.com/photo-1506878206813-92402b8ded23?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://plus.unsplash.com/premium_photo-1677529494239-682591edd525?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac ligula urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum malesuada pulvinar interdum.",
   },
   {
-    id: 4,
+    id: 6,
     title: "LOREM IPSUM 06",
     image:
       "https://images.unsplash.com/photo-1506878206813-92402b8ded23?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
